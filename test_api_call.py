393955117dde18a4d2844b6c8a78b1af98a6a8ea
Ebesoh <ebesoh82@gmail.com>
2025-12-12 22:21:11 +0100

    api_test updated

--- test_api_call.py
+++ test_api_call.py
@@ -1,11 +1,23 @@
 from unittest.mock import patch,Mock
 import api_call
+import requests
+import pytest
 
-def test_fetch_data():
-    fake_response = Mock()
-    fake_response.json.return_value = {"status": "ok"}
+# Test case 1: Successful API Response
+def test_fetch_data_success():
 
-    with patch("api_call.requests.get", return_value = fake_response):
+       # Create a fake response object#
+       # Arrange
+       fake_response = Mock()
+       expected_data = {"key": "value"}
+       fake_response.json.return_value = expected_data
+
+       #Patch requests.get so it returns the fake response#
+       #Act
+       with patch("api_call.requests.get", return_value = fake_response) as mock_get:
         result = api_call.fetch_data()
 
-    assert result == {"status":"ok"}
+       # Assert
+       mock_get.assert_called_once_with("https://api.example.com/data") # Check that requests.get was called once with the correct URL
+       assert result == {"key": "value"} # Check that the function returns the expected JSON
+
